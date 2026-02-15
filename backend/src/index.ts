80243d5d628e76c9228d0e70c29568517019d41b
louis-stintzy <louis.stintzy@gmail.com>
2026-02-16 00:24:10 +0100

    feat(backend): add global middlewares for cookies and body parsing

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,16 @@
 import express, { Request, Response } from 'express';
+import cookieParser from 'cookie-parser';
 const app = express();
-const port = 3000;
+const PORT = 3000;
 
-app.get("/", (_req: Request, res: Response) => {
-  res.send("Hello World!");
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (_req: Request, res: Response) => {
+  res.send('Hello World!');
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+app.listen(PORT, () => {
+  console.log(`➡️  Server running on port ${PORT}`);
 });
