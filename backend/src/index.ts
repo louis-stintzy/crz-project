b817b2b7171c7a27566afcd08ea83521942136f6
louis-stintzy <louis.stintzy@gmail.com>
2026-04-03 08:08:34 +0200

    feat(api): implement custom error handling with AppError class and middleware

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
 import v1Router from './routes/v1';
+import errorHandler from './middlewares/errorHandler';
+
 const app = express();
 const PORT = 3000;
 
@@ -9,6 +11,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/api/v1', v1Router);
+
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`➡️  Server running on port ${PORT}`);
