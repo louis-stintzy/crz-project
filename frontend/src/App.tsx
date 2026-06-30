eb329c0a49e57659f24069b8f3563f5ac0dff527
louis-stintzy <louis.stintzy@gmail.com>
2026-07-01 01:23:46 +0200

    feat(auth-frontend): fetch current user

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,30 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ClosetManager from "./components/ClosetManager";
 import LoginPage from "./components/LoginPage";
 import type { AppUserPublic } from "./types/appUser.types";
+import { appUserService } from "./services/appUser.service";
 
 function App() {
+  const [isLoading, setIsLoading] = useState<boolean>(true);
   const [currentUser, setCurrentUser] = useState<AppUserPublic | null>(null);
+
+  useEffect(() => {
+    const fetchCurrentUser = async () => {
+      try {
+        const user: AppUserPublic = await appUserService.getMe();
+        setCurrentUser(user);
+      } catch {
+        setCurrentUser(null);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    fetchCurrentUser();
+  }, []);
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
 
   return (
     <div>
